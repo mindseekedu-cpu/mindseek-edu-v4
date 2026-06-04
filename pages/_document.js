a4b0b91ead3e748656894e5ab1a87bba9f402601
mindseekedu-cpu <mindseek.edu@gmail.com>
2026-06-04 10:16:32 +0700

    fix(deploy): hapus _document.js custom dan hapus file add.js yang tidak digunakan

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      midtransClientKey: process.env.MIDTRANS_CLIENT_KEY || '',
-      midtransSnapUrl:
-        String(process.env.MIDTRANS_IS_PRODUCTION || '').toLowerCase() === 'true'
-          ? 'https://app.midtrans.com/snap/snap.js'
-          : 'https://app.sandbox.midtrans.com/snap/snap.js',
-    }
-  }
-
-  render() {
-    const clientKey = this.props.midtransClientKey
-    const snapUrl = this.props.midtransSnapUrl
-
-    return (
-      <Html lang="id">
-        <Head>
-          {clientKey ? (
-            <script src={snapUrl} data-client-key={clientKey} async></script>
-          ) : null}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="id">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
