fa47beef6cbe963a897c14382b076ef0bedb9b39
mindseekedu-cpu <mindseek.edu@gmail.com>
2026-06-04 18:54:52 +0700

    fix: tambah script Midtrans

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const clientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || process.env.MIDTRANS_CLIENT_KEY;
+  const isProduction = process.env.MIDTRANS_IS_PRODUCTION === 'true';
+  const snapUrl = isProduction
+    ? 'https://app.midtrans.com/snap/snap.js'
+    : 'https://app.sandbox.midtrans.com/snap/snap.js';
+
   return (
     <Html lang="id">
-      <Head />
+      <Head>
+        {clientKey && (
+          <script src={snapUrl} data-client-key={clientKey} async />
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
